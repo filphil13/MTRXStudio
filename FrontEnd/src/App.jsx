c012b584d010a428720fd464617d24051c159960
All Jay Gaco <alljaygaco@hotmail.com>
2026-03-08 00:36:24 -0500

    refactor: enhance code readability and structure in App component

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,81 +1,145 @@
-
-import './App.css'
-import Title from './components/Title'
-import Introduction from './components/Introduction'
-import Portfolio from './components/Portfolio'
-import ContactUs from './components/ContactUs'
-import Navbar from './components/Navbar'
-import { useRef } from 'react';
-import Background from './components/Background'
+import "./App.css";
+import Title from "./components/Title";
+import Introduction from "./components/Introduction";
+import Portfolio from "./components/Portfolio";
+import ContactUs from "./components/ContactUs";
+import Navbar from "./components/Navbar";
+import { useRef } from "react";
+import Background from "./components/Background";
 
 function App() {
+  const TitleRef = useRef(null);
+  const IntroRef = useRef(null);
+  const PortfolioRef = useRef(null);
+  const ContactRef = useRef(null);
 
-    const TitleRef = useRef(null);
-    const IntroRef = useRef(null);
-    const PortfolioRef = useRef(null);
-    const ContactRef = useRef(null);
-
-
-    const scrollToSection = (ref) => {
+  const scrollToSection = (ref) => {
     if (ref.current) {
       ref.current.scrollIntoView({
-        behavior: 'smooth',
-        block: 'start',
+        behavior: "smooth",
+        block: "start",
       });
     }
   };
 
-    return (
-          <>
-            <Background id="backbg"/>
-            <div className=" font-roboto justify-end bg-transparent">
-                <div ref={TitleRef}>
-                    <Title  />
-                </div>
-                <div className="w-full flex justify-end px-6">
-                  <button type="button" onClick={() => scrollToSection(IntroRef)} className='bg-transparent border-none my-6'>
-                    <svg className='w-16 h-16' dataSlot="icon" fill="none" strokeWidth={1.0} stroke="#FFFFFF" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-                      <path strokeLinecap="round" strokeLinejoin="round" d="m9 12.75 3 3m0 0 3-3m-3 3v-7.5" />
-                    </svg>
-                  </button>
-                </div>
+  return (
+    <>
+      <Background id="backbg" />
+      <div className=" font-roboto justify-end bg-transparent">
+        <div ref={TitleRef}>
+          <Title />
+        </div>
+        <div className="w-full flex justify-end px-6">
+          <button
+            type="button"
+            onClick={() => scrollToSection(IntroRef)}
+            className="bg-transparent border-none my-6"
+          >
+            <svg
+              className="w-16 h-16"
+              dataSlot="icon"
+              fill="none"
+              strokeWidth={1.0}
+              stroke="#FFFFFF"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+              aria-hidden="true"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="m9 12.75 3 3m0 0 3-3m-3 3v-7.5"
+              />
+            </svg>
+          </button>
+        </div>
 
-                <div ref={IntroRef}>
-                  <Introduction />
-                  <div className="w-full flex justify-end px-6">
-                  <button type="button" onClick={() => scrollToSection(PortfolioRef)} className='bg-transparent border-none my-6'>
-                    <svg className='w-16 h-16' dataSlot="icon" fill="none" strokeWidth={1.0} stroke="#FFFFFF" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-                      <path strokeLinecap="round" strokeLinejoin="round" d="m9 12.75 3 3m0 0 3-3m-3 3v-7.5" />
-                    </svg>
-                  </button>
-                </div>
-                </div>
-                
-                <div ref={PortfolioRef}>
-                  <Portfolio />
-                  <div className="w-full flex justify-end px-6">
-                  <button type="button" onClick={() => scrollToSection(ContactRef)} className='bg-transparent border-none my-6'>
-                    <svg className='w-16 h-16' dataSlot="icon" fill="#FFFFFF" strokeWidth={1.0} stroke="#FFFFFF" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-                      <path strokeLinecap="round" strokeLinejoin="round" d="m9 12.75 3 3m0 0 3-3m-3 3v-7.5" />
-                    </svg>
-                  </button>
-                </div>
-                </div>
-                
-                <div ref={ContactRef}>
-                  <ContactUs />
-                  <div className="w-full flex justify-end px-6">
-                  <button type="button" onClick={() => scrollToSection(TitleRef)} className='bg-transparent border-none my-6'>
-                    <svg className='w-16 h-16' dataSlot="icon" fill="#FFFFFF" strokeWidth={1.0} stroke="#FFFFFF" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="m15 11.25-3-3m0 0-3 3m3-3v7.5" />
-                    </svg>
-                  </button>
-                </div>
-                </div>
+        <div ref={IntroRef}>
+          <Introduction />
+          <div className="w-full flex justify-end px-6">
+            <button
+              type="button"
+              onClick={() => scrollToSection(PortfolioRef)}
+              className="bg-transparent border-none my-6"
+            >
+              <svg
+                className="w-16 h-16"
+                dataSlot="icon"
+                fill="none"
+                strokeWidth={1.0}
+                stroke="#FFFFFF"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+                aria-hidden="true"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="m9 12.75 3 3m0 0 3-3m-3 3v-7.5"
+                />
+              </svg>
+            </button>
+          </div>
+        </div>
 
-            </div>
-          </>
-    )
+        <div ref={PortfolioRef}>
+          <Portfolio />
+          <div className="w-full flex justify-end px-6">
+            <button
+              type="button"
+              onClick={() => scrollToSection(ContactRef)}
+              className="bg-transparent border-none my-6"
+            >
+              <svg
+                className="w-16 h-16"
+                dataSlot="icon"
+                fill="#FFFFFF"
+                strokeWidth={1.0}
+                stroke="#FFFFFF"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+                aria-hidden="true"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="m9 12.75 3 3m0 0 3-3m-3 3v-7.5"
+                />
+              </svg>
+            </button>
+          </div>
+        </div>
+
+        <div ref={ContactRef}>
+          <ContactUs />
+          <div className="w-full flex justify-end px-6">
+            <button
+              type="button"
+              onClick={() => scrollToSection(TitleRef)}
+              className="bg-transparent border-none my-6"
+            >
+              <svg
+                className="w-16 h-16"
+                dataSlot="icon"
+                fill="#FFFFFF"
+                strokeWidth={1.0}
+                stroke="#FFFFFF"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+                aria-hidden="true"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="m15 11.25-3-3m0 0-3 3m3-3v7.5"
+                />
+              </svg>
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
