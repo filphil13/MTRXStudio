1d4ea7b7f65872b4e75db38c897506f3063e749d
All Jay Gaco <alljaygaco@hotmail.com>
2026-03-14 21:51:09 -0400

    Remove CursorTrail component to streamline mouse interaction effects

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -4,13 +4,11 @@ import Introduction from "./components/Introduction";
 import Portfolio from "./components/Portfolio";
 import ContactUs from "./components/ContactUs";
 import Background from "./components/Background";
-import CursorTrail from "./components/CursorTrail";
 
 function App() {
 	//sometimes i touch my pp at night
 	return (
 		<>
-			<CursorTrail />
 			<Background id="backbg" />
 			<Title />
 			<Introduction />
