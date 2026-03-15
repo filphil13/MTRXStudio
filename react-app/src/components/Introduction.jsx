ed5803a61035685bf1e739e81c6b2cafb11c8df2
All Jay Gaco <alljaygaco@hotmail.com>
2026-03-14 22:08:22 -0400

    Update gradient colors in Introduction component for improved visual appeal

--- react-app/src/components/Introduction.jsx
+++ react-app/src/components/Introduction.jsx
@@ -7,7 +7,7 @@ function Introduction() {
 			className=" min-h-[calc(100vh-7rem)] text-gray-500"
 		>
 			<h2 className="mb-4 text-6xl font-extrabold font-kode text-gray-900 dark:text-white  md:text-5xl lg:text-6xl">
-				<span className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">
+				<span className="text-transparent bg-clip-text bg-gradient-to-r from-cyber-purple via-cyber-accent to-cyber-glow">
 					What
 				</span>{" "}
 				We Do
